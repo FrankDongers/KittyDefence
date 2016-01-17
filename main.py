d6aa9f88057b533bd61f253a2e7ccbd477023d2c
illuap <paul.j.li.97@gmail.com>
2016-01-16 19:10:33 -0500

    added working kv file

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import kivy
-kivy.require('1.0.6') # replace with your current kivy version !
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.widget import Widget
 
+
+class KDGame(Widget):
+	pass
 
 class MyApp(App):
-
-    def build(self):
-        return Label(text='Hello world')
-
+	def build(self):
+		return KDGame()
 
 if __name__ == '__main__':
-    MyApp().run()
+	MyApp().run()
